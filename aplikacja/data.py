0b2d6e73717f301889ffb93bab68fa7d16bcd055
Mateusz <mateusz.nabaglo@edu.uekat.pl>
2023-05-22 18:11:28 +0200

    Frontend improvment

--- aplikacja/data.py
+++ aplikacja/data.py
@@ -9,12 +9,14 @@ from aplikacja.db import get_db
 bp = Blueprint('data', __name__, url_prefix='/data')
 
 @bp.route('/all')
+@login_required
 def all():
     db = get_db()
     all_data = db.execute(
         'SELECT p.id, glucose, activity, info, created, author_id, username'
         ' FROM data p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
+        ' WHERE p.author_id = ?'
+        ' ORDER BY created DESC',(g.user['id'],)
     ).fetchall()
     return render_template('data/all.html', all_data=all_data)
 
@@ -43,3 +45,47 @@ def create():
             return redirect(url_for('data.all'))
 
     return render_template('data/create.html')
+
+def get_single_data(id, check_author=True):
+    s_data = get_db().execute(
+        'SELECT p.id, glucose, activity, info, created, author_id, username'
+        ' FROM data p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if s_data is None:
+        abort(404, f"Entry id {id} doesn't exist.")
+
+    if check_author and s_data['author_id'] != g.user['id']:
+        abort(403)
+
+    return s_data
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    data = get_single_data(id)
+
+    if request.method == 'POST':
+        glucose = request.form['glucose']
+        activity = request.form['activity']
+        info = request.form['info']
+        error = None
+
+        if not glucose:
+            error = 'Glucose is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE data SET glucose = ?, activity = ?, info = ?'
+                ' WHERE id = ?',
+                (glucose, activity, info, id)
+            )
+            db.commit()
+            return redirect(url_for('data.all'))
+
+    return render_template('data/update.html', data=data)
