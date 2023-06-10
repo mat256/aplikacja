fc8d0b572f99aa6b1c14587e108e2312ce17381f
Mateusz <mateusz.nabaglo@edu.uekat.pl>
2023-06-11 00:08:24 +0200

    fixes

--- aplikacja/dashboard.py
+++ aplikacja/dashboard.py
@@ -8,7 +8,7 @@ from aplikacja.db import get_db
 
 from bokeh.embed import components
 from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource, HoverTool, TableColumn, DataTable, DateFormatter,HTMLTemplateFormatter
+from bokeh.models import ColumnDataSource, HoverTool, TableColumn, DataTable, DateFormatter,HTMLTemplateFormatter,DatetimeTickFormatter,BoxAnnotation, Toggle
 from bokeh.io import show
 
 import random
@@ -24,9 +24,7 @@ import os
 
 bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
-@bp.route('/')
-@login_required
-def dashboard():
+def create_avg_data():
     db = get_db()
     all_data = db.execute(
         'SELECT p.id, glucose, activity, info, custom_date, created, stat, author_id'
@@ -35,61 +33,90 @@ def dashboard():
         ' ORDER BY created DESC', (g.user['id'],)
     ).fetchall()
     # print(all_data[0][0])
-    tab = pd.read_sql('SELECT p.id, glucose, activity, info, custom_date, created, stat, author_id'
-                      ' FROM data p JOIN user u ON p.author_id = u.id'
-                      ' ORDER BY created DESC', db)
+    df = pd.DataFrame(all_data,
+                      columns=['id', 'glucose', ' activity', 'info', 'custom_date', 'created', 'stat', 'author_id'])
+    # print(df)
+    df = df.filter(['glucose', 'custom_date'], axis=1)
+    #print(df)
+    #new=df.groupby(pd.Grouper(key='custom_date', axis=0, freq='15min', sort=True)).mean()
+    #new = df.groupby(df["custom_date"].dt.hour)["glucose"].mean()
+    #new['date']=list(new.index.values)
+
+    df1 = df.set_index('custom_date')
+    #print(df1)
+    new=pd.DataFrame()
+    #for i in pd.timedelta_range(start='1 day', end='2 days', freq='15min'):
+        #print(str(i).split()[-1])
+    #new["glucose"]=[df.between_time('0:15', str(x).split()[-1]) for x in pd.timedelta_range(start='1 day', end='2 days', freq='15min')]
+
+    #print(df1.between_time('00:00:00', '01:00:00')['glucose'].mean())
+    timing = [str(x).split()[-1] for x in pd.timedelta_range(start='1 day', end='2 days', freq='15min')]
+    new['time']=timing[:-1]
+    new['time'] = new['time'].apply(lambda x: f"2022-06-10 {x}")
+    new['time'] = pd.to_datetime(new['time'])
+    #print(timing)
+    new["glucose"] = [df1.between_time(timing[x], timing[x+1])['glucose'].mean() for x in range(len(timing)-1)]
+    new.fillna(method="bfill", inplace=True)
+    #print(new)
+    new['glucose'] = new['glucose'].astype(int)
+    #print(list(new.columns.values))
+    #print(list(new.index.values))
+    #print(type(new['time'][0]))
+    #print(new.to_string())
+    return new
+
+@bp.route('/')
+@login_required
+def dashboard():
+    create_avg_data()
+    db = get_db()
+    all_data = db.execute(
+        'SELECT p.id, glucose, activity, info, custom_date, created, stat, author_id'
+        ' FROM data p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.author_id = ?'
+        ' ORDER BY created DESC', (g.user['id'],)
+    ).fetchall()
+    # print(all_data[0][0])
     df = pd.DataFrame(all_data,
                       columns=['id', 'glucose', ' activity', 'info', 'custom_date', 'created', 'stat', 'author_id'])
     # print(df)
     chart_data = df.filter(['glucose', 'custom_date'], axis=1)
     chart_data.sort_values(by='custom_date', ascending=False, inplace=True)
-    source = ColumnDataSource(chart_data)
+    source = ColumnDataSource(create_avg_data())
     source2 = ColumnDataSource(chart_data)
 
-    p1 = figure(height=350, sizing_mode="stretch_width")
 
-    TOOLTIPS = [
-        ("index", "@glucose"),
-        ("desc", "@custom_date{%F}"),
-    ]
     hover_tool = HoverTool(
-        tooltips=[("index", "@glucose"),
-        ("desc", "@custom_date{%T}")], mode='vline', formatters={"@custom_date": "datetime"}
-    )
-    p1.circle(
-        [i for i in range(10)],
-        [random.randint(1, 50) for j in range(10)],
-        size=20,
-        color="navy",
-        alpha=0.5
+        tooltips=[("Glucose", "@glucose"),
+        ("Time", "@time{%T}")], mode='vline', formatters={"@time": "datetime"}
     )
 
-    # Second Chart - Line Plot
-    language = ['Python', 'JavaScript', 'C++', 'C#', 'Java', 'Golang']
-    popularity = [85, 91, 63, 58, 80, 77]
 
-    p2 = figure(
-        x_range=language,
-        height=350,
-        title="Popularity",
-    )
-    p2.vbar(x=language, top=popularity, width=0.5)
-    p2.xgrid.grid_line_color = None
-    p2.y_range.start = 0
-
-    # Third Chart - Line Plot
     p3 = figure( sizing_mode="scale_width", x_axis_type="datetime")
+
     p3.add_tools(hover_tool)
     p3.line(
-        x='custom_date', y='glucose', source=source,
-        line_width=2,
-        color="olive",
+        x='time', y='glucose', source=source,
+        line_width=3,
+        color="darkblue",
         alpha=0.5
     )
 
-    script1, div1 = components(p1)
-    script2, div2 = components(p2)
-    script3, div3 = components(p3)
+    """super_low_box = BoxAnnotation(top=60, fill_alpha=0.1, fill_color='bisque')
+    low_box = BoxAnnotation(bottom=60, top=75, fill_alpha=0.1, fill_color='cornsilk')
+    mid_box = BoxAnnotation(bottom=75, top=140, fill_alpha=0.1, fill_color='honeydew')
+    high_box = BoxAnnotation(bottom=140, top=180, fill_alpha=0.1, fill_color='cornsilk')
+    super_high_box = BoxAnnotation(bottom=180, fill_alpha=0.1, fill_color='bisque')
+    p3.add_layout(super_low_box)
+    p3.add_layout(low_box)
+    p3.add_layout(mid_box)
+    p3.add_layout(high_box)
+    p3.add_layout(super_high_box)"""
+
+    #p3.xaxis[0].formatter = DatetimeTickFormatter(months="%b %Y")
+
+    script1, div1 = components(p3)
+
 
     formatter = HTMLTemplateFormatter()
 
@@ -102,11 +129,11 @@ def dashboard():
     #show(myTable)
     #print(myTable)
     #print(components(myTable))
-    script4, div4 = components(myTable)
+    script2, div2 = components(myTable)
     #print(div3)
     #print(div4)
     return render_template(
         'dashboard/main.html',
-        script=[script1, script2, script3, script4],
-        div=[div1, div2, div3, div4],
+        script=[script1, script2],
+        div=[div1, div2],
     )
